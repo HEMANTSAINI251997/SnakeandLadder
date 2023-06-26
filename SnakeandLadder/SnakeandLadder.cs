5098602bb22348f9787682b17373e571af7f5135
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-06-26 15:02:03 +0530

    [HEMANT]Add: Repeat till the player reaches the win pos-100

--- SnakeandLadder/SnakeandLadder.cs
+++ SnakeandLadder/SnakeandLadder.cs
@@ -12,25 +12,37 @@ namespace SnakeandLadder
         public void Checksnakeandladder()
         {
             Console.WriteLine("Start Position = " + position);
-            Random random = new Random();   
-            int num = random.Next(1,7);
-            Console.WriteLine("Player roll the dice and get the number = " + num);
-            int option = random.Next(0,3);
-            if (option == 0)
+            Random random = new Random();
+            while (position < 100)
             {
-                Console.WriteLine("They are NO Play, Postion = " + position);
-                
+                int num = random.Next(1, 7);
+                Console.WriteLine("Player roll the dice and get the number = " + num);
+                int option = random.Next(0, 3);
+                if (option == 0)
+                {
+                    Console.WriteLine("They are NO Play, Postion = " + position);
+
+                }
+                else if (option == 1)
+                {
+                    position = position + num;
+                    Console.WriteLine("LADDER - Position Add By = " + position);
+                }
+                else
+                {
+                    position = position - num;
+                    if (position < 0)
+                    {
+                        position = 0;
+                    }
+
+                    Console.WriteLine("SNAKE - Position Down By = " + num + "   Postion = " + position);
+                    
+                    
+                }
+               
             }
-            else if (option == 1)
-            {
-                position = position + num;
-                Console.WriteLine("LADDER - Position Add By = " + position );
-            }
-            else
-            {
-                position=position - num;
-                Console.WriteLine("SNAKE - Position Down By = " + position);
-            }
+            Console.WriteLine("Player reaches the winning position 100!");
         }
     }
 }
