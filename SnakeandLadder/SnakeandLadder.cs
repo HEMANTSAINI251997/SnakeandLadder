8384d7197f737985bdcf41935d412197017b11e0
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-06-26 14:41:23 +0530

    [HEMANT]Add:Check for : THey are No Play , Ladder or Snake

--- SnakeandLadder/SnakeandLadder.cs
+++ SnakeandLadder/SnakeandLadder.cs
@@ -15,6 +15,22 @@ namespace SnakeandLadder
             Random random = new Random();   
             int num = random.Next(1,7);
             Console.WriteLine("Player roll the dice and get the number = " + num);
+            int option = random.Next(0,3);
+            if (option == 0)
+            {
+                Console.WriteLine("They are NO Play, Postion = " + position);
+                
+            }
+            else if (option == 1)
+            {
+                position = position + num;
+                Console.WriteLine("LADDER - Position Add By = " + position );
+            }
+            else
+            {
+                position=position - num;
+                Console.WriteLine("SNAKE - Position Down By = " + position);
+            }
         }
     }
 }
