c92cb158616f77dd1a62b932b7b647cb96b30f8c
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-06-26 14:15:30 +0530

    [HEMANT]Add: Single player at start postion 0

---
+++ SnakeandLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SnakeandLadder snakeandLadder = new SnakeandLadder();   
+            snakeandLadder.Checksnakeandladder();
+        }
+    }
+}
---
+++ SnakeandLadder/SnakeandLadder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadder
+{
+    public class SnakeandLadder
+    {
+        int position = 0;
+        public void Checksnakeandladder()
+        {
+            Console.WriteLine("Start Position = " + position);
+        }
+    }
+}
